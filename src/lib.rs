3a39b5847e153913a40355af5f4a3ea28a0be44d
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2022-11-15 17:28:38 -0500

    refactor(Config): rewrite from_args to use iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-mod search;
+pub mod search;
 
 use std::error::Error;
 use std::{fs, env};
 
 pub fn app(cfg: Config) -> Result<(), Box<dyn Error>> {
     println!("\nSearching for << {:?} >>", &cfg.query);
-    println!("on file: << {:?} >>", &cfg.filepath);
+    println!("on file: << {:?} >>", &cfg.file_path);
 
   // Obtenemos texto del archivo
-  let file_txt = fs::read_to_string(&cfg.filepath)?;
+  let file_txt = fs::read_to_string(&cfg.file_path)?;
 
   let results = search::search_in_string(&cfg.query, &file_txt);
 
@@ -20,22 +20,32 @@ pub fn app(cfg: Config) -> Result<(), Box<dyn Error>> {
 
 pub struct Config {
     pub query: String,
-    pub filepath: String,
+    pub file_path: String,
 }
 
 impl Config {
     pub fn from_args() -> Result<Config, &'static str> {
-        let args: Vec<String> = env::args().collect();
+        let mut args = env::args();
 
-        if args.len() != 3 {
-            return Err("You must provide 2 arguments");
-        }
+        // if args.len() != 3 {
+        //     return Err("You must provide 2 arguments");
+        // }
 
-        let cfg = Config{
-            query: args[1].clone(),
-            filepath: args[2].clone()
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
         };
 
-        Ok(cfg)
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        Ok(Config{
+            query,
+            file_path,
+        })
     }
 }
