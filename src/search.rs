9339693d39057af605db640eefe6110572000561
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2022-11-15 17:32:41 -0500

    docs(search): document function search_in_string

--- src/search.rs
+++ src/search.rs
@@ -1,9 +1,23 @@
+/// Search for a query in a string
+///
+/// # Examples
+///
+/// ```
+/// let text = "\
+/// several lines
+/// of text
+/// to search for".to_string();
+///
+/// let results = minigrep::search::search_in_string("se", &text);
+///
+/// assert_eq!(2, results.len());
+/// ```
 pub fn search_in_string<'a>(query: &'a str, text: &'a String) -> Vec<&'a str> {
     text
         .lines()
         .filter(|line| line.contains(query))
         .collect()
-    }
+}
 
 
 #[cfg(test)]
